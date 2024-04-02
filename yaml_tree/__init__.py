519e43711b292c82a8f5d0166c63daff86adb1a6
Edwin Saul <edwinsaulpm@gmail.com>
2024-04-02 13:22:18 -0500

    data structure configured

--- yaml_tree/__init__.py
+++ yaml_tree/__init__.py
@@ -1,5 +1,7 @@
 # yaml_tree by Edwin Saul
 
+from os.path import isdir,isfile, join
+
 try:     from printTree  import printTree
 except:  from .printTree import printTree 
 
@@ -14,32 +16,99 @@ class tree:
     def __init__(self):
         # path  for scan info
         self.__path=""
+        self.__file=""
         self.__knowPath=False
         # cell var counter
         self.__cellCounter=-1
         # cell oneFile or multiple files
         self.__OneFile=False
         # data in tree cell
-        self.__cellName  =[]  # cell name 
-        self.__cellValue =[]  # cell Value
-        self.__cellStatus=[]  # true if exists false if not
-        self.__cellFather=[]  # code of father
-        self.__cellChilds=[]  # list of code of childs
-        self.__cellIsPath=[]  # true if is path false if is var or file 
-        self.__cellIsFile=[]  # true if is file, false if is path or var
+        self.__cellName  =[]       # cell name 
+        self.__cellValue =[]       # cell Value
+        self.__cellStatus=[]       # true if exists false if not
+        self.__cellHasContent=[]   # true if has content false is has childs
+        self.__cellFather=[]       # code of father
+        self.__cellChilds=[]       # list of code of childs
+        self.__cellComments=[]     # list of code of childs
+        self.__cellTag=[]          # tag of cell
         # first childs : childs of -1
         self.__firstChilds=[]
+        # extensions for add elements
+        self.__extensions=[".csv"] # extensions for add elements
         # data alias
         self.__alias={}       # "alias", []
+
 
     def print(self): printTree(self)
     def read(self):  readTree(self)
     def write(self): writeTree(self)
 
-    def get_path(self):    return self.__path
-    def get_knowPath(self):return self.__knowPath
-    def get_data(self):    return [self.__cellName,self.__cellValue,self.__cellStatus,self.__cellFather,self.__cellChilds,self.__cellIsPath,self.__cellIsFile,self.__firstChilds]
-    def get_alias(self):   return self.__alias
+
+    def get_path(self):      return self.__path
+    def get_file(self):      return self.__file
+    def get_knowPath(self):  return self.__knowPath
+    def get_data(self):      return [self.__cellName,self.__cellValue,self.__cellStatus,self.__cellHasContent,self.__cellFather,self.__cellChilds,self.__cellComments,self.__cellTag,self.__firstChilds]
+    def get_extensions(self):return self.__extensions
+    def get_alias(self):     return self.__alias
+
+
+    def get(self,direction):
+       pass
+
+
+    def set(self,direction,value):
+        pass
+
+
+    def make(self,direction,new):
+        codeFather=self.__get_code(direction)
+        self.__raw_create(codeFather,new)
+
+
+    def childs(self,direction):
+        codeFather=self.__get_code(direction)
+        if codeFather==-1:
+            return self.__get_names_by_code(self.__firstChilds)
+        else:
+            return []
+
+
+    def __raw_create(self,codeFather,name):
+        self.__cellCounter+=1
+        self.__cellName.append(str(name)[:])
+        self.__cellValue.append(0)
+        self.__cellStatus.append(True)
+        self.__cellHasContent.append(True)
+        self.__cellFather.append(codeFather)
+        self.__cellChilds.append([])
+        self.__cellComments.append(None)
+        self.__cellTag.append(None)
+        if codeFather==-1: # in root 
+            self.__firstChilds.append(self.__cellCounter)
+        else:
+            self.__cellChilds[codeFather]=self.__cellChilds[codeFather].append(self.__cellCounter)
+            self.__cellHasContent[codeFather]=False
+
+    def __get_names_by_code(self,list_of_codes):
+        names=[]
+        for code in list_of_codes: names.append(self.__cellName[code])
+        return names
+
+
+    def __get_code(self,direction):
+        if len(direction)==0: return -1 # root main
+        elif type(direction)==str:
+            direction=direction.replace("\\","/").split("/")
+            pass
+        elif type(direction)==list:
+            if len(direction)==1:
+                # code on root
+                if direction[0] in self.__get_names_by_code(self.__firstChilds):
+                    pass
+                else: return -1
+            else:
+                pass
+        else: return -1 # root main
 
 
 
@@ -47,8 +116,10 @@ class tree:
 
 if __name__=="__main__": # tests
     a=tree()
+    a.make("","data1")
+    a.make("","data2")
+
+#    a.make("data1","data3")
     a.print()
-
-
-
+#    input()
 
--- yaml_tree/printTree.py
+++ yaml_tree/printTree.py
@@ -1,8 +1,34 @@
 # yaml_tree by: Edwin Saul https://edwinsaul.com
 
+from os import get_terminal_size
+
+
+def printLine(text):
+    text=str(text)
+    console_width = get_terminal_size().columns
+    if len(text)<console_width: print(text)
+    else: print(text[0:console_width-1])
+
+
+def printLineTree(tree,path):
+    childs=tree.childs(path)
+    for x in childs:
+        new_path=path+[x]
+        line=(" "*len(path))+" "+str(x)
+        #-----  cicle on printLineTree --------
+        printLine(line)
+        printLineTree(tree,new_path)
+        #---------------------------------------
 
 def printTree(tree):
     print("path   : "+str(tree.get_path()))
+    print("file   : "+str(tree.get_file()))
     print("know?  : "+str(tree.get_knowPath()))
-    print(tree.get_data())
-    print(tree)
+    print("")
+    printLineTree(tree,[])
+
+# --------------------------------------------------------
+if __name__=="__main__":
+    printLine(100*"------")
+    input()
+
